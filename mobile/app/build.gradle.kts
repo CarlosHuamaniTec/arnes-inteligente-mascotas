b2bce90e7c6f4e6c49993fe0f0068d0bfa7adbb4
CarlosHuamaniTec <carlos.huamani.pi@gmail.com>
2025-05-21 07:59:19 -0500

    Firebase configurado en Vital Paw Mobile con gitignore

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,6 +42,10 @@ android {
 
 dependencies {
 
+    //noinspection UseTomlInstead
+    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
